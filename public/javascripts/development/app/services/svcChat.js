8f0db9f791eb4f2b8a8801eed1df7dd1dd15d5d5
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-07 20:51:55 +0000

    arreglos en webRTC con simpleWEBRTC

---
+++ public/javascripts/development/app/services/svcChat.js
@@ -0,0 +1,6 @@
+senseiApp.service('svcChat',['$firebase',function($firebase){
+  var ref = new Firebase('http://myfirechat.firebaseio.com')
+  this.sync = $firebase(ref)
+  this.rooms = ref.child("rooms")
+  this.firebase = $firebase
+}])
